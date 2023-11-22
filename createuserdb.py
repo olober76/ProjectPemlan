58e5684c66f30ee1c8fa559a622f5c19e197db18
Fawwaz Anrico <fawwazanricopurnomo@gmail.com>
2023-11-22 21:15:52 +0700

    perubahan database user

--- createuserdb.py
+++ createuserdb.py
@@ -5,7 +5,7 @@ import os
 def create_db():
   if not os.path.isfile('users.csv'):
     with open('users.csv', 'w') as csvfile:
-      fieldnames = ['username', 'password']
+      fieldnames = ['username', 'password','tiket_film','jumlah']
       writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
       writer.writeheader()
   else:
