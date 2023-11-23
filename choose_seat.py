b0efdcfb0af1ee9237442afba8e4cb22c113b30d
olober76 <dutakupri@gmail.com>
2023-11-23 12:18:34 +0700

    refisi untuk choose_seat.py

--- choose_seat.py
+++ choose_seat.py
@@ -1,7 +1,8 @@
 import csv
 import tkinter as tk
+from tkinter import messagebox
 
-def choose_seat(selected_movie):
+def choose_seat(selected_movie, username, password):
     filename = '{}.csv'.format(selected_movie)
     with open(filename, 'r') as file:
         reader = csv.reader(file)
@@ -9,24 +10,52 @@ def choose_seat(selected_movie):
 
         root = tk.Tk()
         seat_buttons = [[None for _ in range(len(seats[0]))] for _ in range(len(seats))]
+        cart = []  # Keranjang untuk menyimpan informasi kursi yang ditekan
 
         # Create a Seat Selection GUI
         for i in range(len(seats)):
             for j in range(len(seats[i])):
                 text = seats[i][j]
-                seat_buttons[i][j] = tk.Button(root, text=text, command=lambda i=i, j=j: book_seat(seats, seat_buttons, selected_movie, i, j))
+                seat_buttons[i][j] = tk.Button(root, text=text, command=lambda i=i, j=j: book_seat(seats, selected_movie, seat_buttons, i, j, cart))
                 seat_buttons[i][j].grid(row=i, column=j)
 
+        def save_and_confirm():
+            confirmation = messagebox.askokcancel("Confirm", "Are you sure you want to confirm? Changes will be saved.")
+            if confirmation:
+                save_data(selected_movie, seats)
+                save_cart(cart, selected_movie, username, password)  # Simpan keranjang ke file CSV
+                root.destroy()
+
+        confirm_button = tk.Button(root, text="Confirm", command=save_and_confirm)
+        confirm_button.grid(row=len(seats) + 1, columnspan=len(seats[0]))  # Place the Confirm button below the seats
+
+        def on_closing():
+            if messagebox.askokcancel("Confirm", "Are you sure you want to close? Changes will be saved."):
+                root.destroy()
+                save_data(selected_movie, seats)
+
+        root.protocol("WM_DELETE_WINDOW", on_closing)
         root.mainloop()
 
+def save_data(selected_movie, seats):
+    with open('{}.csv'.format(selected_movie), 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(seats)
+
+# Save Cart to CSV
+def save_cart(cart, selected_movie, username, password):
+    with open('users.csv', 'a', newline='') as file:  # 'a' means append mode
+        writer = csv.writer(file)
+        for seat in cart:
+            writer.writerow([username, password, selected_movie, seat])  # Menulis data
+
 # Handle Seat Selection
-def book_seat(seats, seat_buttons, selected_movie, i, j):
-    if seats[i][j] == 'X':  # Only allow booking if the seat is not yet booked
-        seats[i][j] = '1'
-        with open('{}.csv'.format(selected_movie), 'w', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerows(seats)
-        seat_buttons[i][j].config(text='1')
+def book_seat(seats, selected_movie, seat_buttons, i, j, cart):
+    seats[i][j] = 'X'
+    seat_buttons[i][j].config(text='X')  # Update the button's text
+    cart.append((i, j))  # Tambahkan informasi kursi ke keranjang
 
 if __name__ == "__main__":
-    choose_seat('barbie')
+    username = "user1"  # Contoh username
+    password = "password1"  # Contoh password
+    choose_seat('barbie', username, password)
