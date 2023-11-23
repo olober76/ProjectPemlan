4dd0f9d8e0c5498b96aa519341dc3e31ba2e984d
olober76 <dutakupri@gmail.com>
2023-11-23 09:06:25 +0700

    refisi untuk choose_seat.py

--- choose_seat.py
+++ choose_seat.py
@@ -1,7 +1,6 @@
 import csv
 import tkinter as tk
 
-
 def choose_seat(selected_movie):
     filename = '{}.csv'.format(selected_movie)
     with open(filename, 'r') as file:
@@ -15,19 +14,19 @@ def choose_seat(selected_movie):
         for i in range(len(seats)):
             for j in range(len(seats[i])):
                 text = seats[i][j]
-                seat_buttons[i][j] = tk.Button(root, text=text, command=lambda i=i, j=j: book_seat(seats,selected_movie,i, j))
+                seat_buttons[i][j] = tk.Button(root, text=text, command=lambda i=i, j=j: book_seat(seats, seat_buttons, selected_movie, i, j))
                 seat_buttons[i][j].grid(row=i, column=j)
 
         root.mainloop()
 
 # Handle Seat Selection
-def book_seat(seats,selected_movie, i, j):
-    seats[i][j] = 'X'
-    with open('{}.csv'.format(selected_movie), 'w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerows(seats)
-    seats[i][j].config(text='X')
-
+def book_seat(seats, seat_buttons, selected_movie, i, j):
+    if seats[i][j] == 'X':  # Only allow booking if the seat is not yet booked
+        seats[i][j] = '1'
+        with open('{}.csv'.format(selected_movie), 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerows(seats)
+        seat_buttons[i][j].config(text='1')
 
 if __name__ == "__main__":
     choose_seat('barbie')
